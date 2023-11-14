e8a9f3a853e99826a063b23c89fd4ff87e59dce0
Justin Chu <justinchu24.com>
2023-11-13 23:03:54 -0500

    refactored controller and model

--- backend/controller/controller.go
+++ backend/controller/controller.go
@@ -1,32 +1,55 @@
 package controller
 import (
 	"fmt"
+	"os"
+	"database/sql"
+	"net/http"
+	"encoding/json"
 	"angular_go/backend/models"
-	"strconv"
 )
 
-
-func getAllAlgos() {
-	fmt.Printf("got / request!\n")
-
-  // Get the database connection.
-  models.ConnectToDB()
-
-  // Get the algo ID from the request query parameters.
-  algoID, err := strconv.Atoi(r.URL.Query().Get("id"))
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-
-  // Query the database for the algo record with the given ID.
-  var algo Algo
-  err = db.QueryRow("SELECT * FROM algos WHERE id = $1", algoID).Scan(&algo.ID, &algo.Name, &algo.Resource)
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-
-  // Return the algo to the client as JSON.
-  json.NewEncoder(w).Encode(algo)
+func ConnectToDB() {
+	connStr := os.Getenv("DATABASE_URL")
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	return db
 }
+
+func GetAllAlgos(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("got / request!")
+
+	// Get the database connection.
+	db, err := ConnectToDB()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	// Query the database for all Algo records.
+	rows, err := db.Query("SELECT * FROM algos")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	// Iterate over the results and return them to the client.
+	var algos []models.Algo
+	for rows.Next() {
+		var algo models.Algo
+		err := rows.Scan(&algo.ID, &algo.Name, &algo.Resource)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		algos = append(algos, algo)
+	}
+
+	// Return the algos to the client as JSON.
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(algos)
+}
+
--- backend/models/models.go
+++ backend/models/models.go
@@ -1,50 +1,11 @@
 package models
 
-import (
-	"database/sql"
-	"fmt"
-	"os"
-)
-
-func ConnectToDB() {
-	// Create a database connection string.
-	connStr := os.Getenv("DATABASE_URL")
-	// "postgres://kfexssfq:3E-cEMRK3wFxs-kKf872lJWEll7Ts3xh@suleiman.db.elephantsql.com/kfexssfq"
-
-	// Open a connection to the database.
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-			panic(err)
-	}
-
-	// Close the connection when we're done.
-	defer db.Close()
-
-	// Query the database.
-	rows, err := db.Query("SELECT * FROM algos")
-	if err != nil {
-			panic(err)
-	}
-
-	// Iterate over the results.
-	for rows.Next() {
-			var algo Algo
-			err := rows.Scan(&algo.ID, &algo.Name, &algo.Resource)
-			if err != nil {
-					panic(err)
-			}
-			// Print the algo information.
-			fmt.Println(algo.ID, algo.Name, algo.Resource)
-	}
-}
-
 type Algo struct {
 	ID int64
 	// UserID int64
 	Name string
 	Resource string
 }
-
 
 // MAYBE LATER
 
